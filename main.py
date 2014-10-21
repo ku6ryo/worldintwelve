ba85f14af12a57f70e401ef63b54682a1dce9a5b
ku6ryo <ku6ryo@gmail.com>
2014-10-22 00:16:40 +0900

    cleared up the main.py

--- main.py
+++ main.py
@@ -29,6 +29,14 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 
+BREADCRUMB_DICT = {
+  'sanfrancisco': 'San Francisco',
+  'riodejaneiro': 'Rio de Janeiro',
+  'grandma_cooking': 'Grandmas\' Cooking',
+  'test_city_tokyo': 'TEST CITY: Tokyo'
+}
+
+
 CITIES = [
   'sanfrancisco',
   'toronto',
@@ -36,8 +44,8 @@ CITIES = [
   'riodejaneiro',
   'london',
   'stockholm',
-  'munich',
   'paris',
+  'munich',
   'barcelona',
   'istanbul',
   'bangkok',
@@ -203,200 +211,207 @@ CITY_METAS = {
   }
 }
 
-
 PROJECTS = [
-  {'id': 'musicians', 'label': 'Musicians'},
-  {'id': 'soundscape', 'label': 'Soundscape'},
-  {'id': 'treasure_box', 'label': 'Treasure Box'},
-  {'id': 'live', 'label': 'Live'},
-  {'id': 'dates1', 'label': 'Dates Part 1'},
-  {'id': 'dates2', 'label': 'Dates Part 2'},
-  {'id': 'grandma_cooking', 'label': 'Grandma\'s Cooking'},
-  {'id': 'micro_guide', 'label': 'Micro Guide'},
-  {'id': 'misc', 'label': 'Misc'},
-  {'id': 'twelve_questions', 'label': '12 Questions'},
-  {'id': 'bck', 'label': 'BCK'},
-  {'id': 'marketplace', 'label': 'Market Place'}
+        'musicians',
+        'soundscape',
+        'treasure_box',
+        'live', 
+        'dates1', 
+        'dates2', 
+        'grandma_cooking', 
+        'micro_guide', 
+        'misc', 
+        'twelve_questions', 
+        'bck', 
+        'marketplace'
 ]
+
 
 PROJECT_METAS = {
   'musicians': {
+      'id': 'musicians',
+      'label_en': 'Musicians',
       'label': u'アーティスト・ミュージシャン',
       'metas': [u'基本フォーマット：音楽', u'曲数：12曲', u'尺：−−', u'参加人数：12組（バンド・アーティスト）']
   },
   'bck': {
+      'id': 'bck',
+      'label_en': 'BCK',
       'label': 'Blind Cheap Kudos',
       'metas': [u'基本フォーマット：音楽', u'概要フォーマット：映像', u'尺：−−',
           u'曲数：1', u'参加人数：--']
   },
   'dates1': {
+      'id': 'dates1',
+      'label_en': 'Dates Part 1',
       'label': u'デート パート １',
       'metas': [u'基本フォーマット：映像', u'エピソード数：12本', u'尺：５分', u'参加人数：各都市４人']
   },
   'dates2': {
+      'id': 'dates2',
+      'label_en': 'Dates Part 2',
       'label': u'デート パート 2',
       'metas': [u'基本フォーマット：映像', u'エピソード数：12本',
           u'尺：2分24秒', u'参加人数：カップル12組（計24人）']
   },
   'grandma_cooking': {
+      'id': 'grandma_cooking',
+      'label_en': 'Grandma\'s Cooking',
       'label': u'おばあちゃんの手料理',
       'metas': [u'基本フォーマット：映像', u'概要フォーマット：写真・文章',
       u'エピソード数：1', u'参加人数：--']
   },
   'micro_guide': {
+      'id': 'micro_guide',
+      'label_en': 'Micro Guide',
       'label': u'細かすぎて伝わらないトリセツ',
       'metas': [u'基本フォーマット：イラスト・文章', u'概要フォーマット：映像',
           u'参加人数：−−']
   },
   'soundscape': {
+      'id': 'soundscape',
+      'label_en': 'Soundscape',
       'label': u'サウンドスケープ',
       'metas': [u'基本フォーマット：音楽・音像', u'曲数：12曲', u'尺：−−',
       u'参加人数：−−']
   },
   'misc': {
+      'id': 'misc',
+      'label_en': 'Misc',
       'label': u'ザ・その時決める',
       'metas': [u'基本フォーマット：写真・文章', u'概要フォーマット：映像',
           u'エピソード数：1', u'参加人数：--']
   },
   'treasure_box': {
+      'id': 'treasure_box',
+      'label_en': 'Treasure Box',
       'label': u'宝箱バトル',
       'metas': [u'基本フォーマット：写真・文章', u'サブフォーマット：映像',
       u'エピソード数：１回', u'参加人数：自分＋2~3人']
   },
   'twelve_questions': {
+      'id': 'twelve_questions',
+      'label_en': '12 Questions',
       'label': u'１２の質問',
       'metas': [u'基本フォーマット：写真・文章', u'概要フォーマット：映像',
       u'エピソード数：12回', u'参加人数：12人']
   },
   'marketplace': {
+      'id': 'marketplace',
+      'label_en': 'Market Place',
       'label': u'マーケット',
       'metas': [u'基本フォーマット：写真・ウェブ', u'尺：−−',
           u'参加人数：最低12組']
   },
   'live': {
+      'id': 'live',
+      'label': 'Live',
       'label': u'ライブ！',
       'metas': []
   }
 }
 
 
+def GetTemplate(path):
+  return JINJA_ENVIRONMENT.get_template('templates/%s' % path)
+
+
+def WrapWithBaseTemplate(content, dir_list):
+  base_template = GetTemplate('base.html')
+  breadcrumbs = [BREADCRUMB_DICT[bc] if bc in BREADCRUMB_DICT else bc for bc in dir_list]
+  return base_template.render({'content': content, 'dir_list': breadcrumbs})
+
+
 class TopHandler(webapp2.RequestHandler):
     def get(self):
-        base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-        top_template = JINJA_ENVIRONMENT.get_template('templates/top.html')
+        top_template = GetTemplate('/top.html')
         cities = ['tokyo']
         cities.extend(CITIES)
         body = top_template.render({
             'cities': [CITY_METAS[c] for c in cities],
         })
-        self.response.write(base_template.render({'body': body}))
+        self.response.write(WrapWithBaseTemplate(body, []))
 
 
-class CitiesHandler(webapp2.RequestHandler):
-    def get(self, city):
-        base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-        if city != None:
-            frame_template = JINJA_ENVIRONMENT.get_template('templates/cities/frame.html')
-            city_template = JINJA_ENVIRONMENT.get_template('templates/cities/%s.html' % city)
-            city_body = city_template.render()
-            info_template = JINJA_ENVIRONMENT.get_template('templates/cities/info_frame.html')
-            info_body = info_template.render({
-              'description': city_body,
-              'meta': CITY_METAS[city],
-              'city_id': city
-            })
-            body = frame_template.render({
-                'info': info_body,
-                'city_id': city,
-                'cities': [CITY_METAS[c] for c in CITIES]
-            })
-            dir_list = ['cities', city]
-        else:
-            frame_template = JINJA_ENVIRONMENT.get_template('templates/cities/frame.html')
-            body = frame_template.render({
-                'info': '',
-                'city_id': '',
-                'cities': [CITY_METAS[c] for c in CITIES],
-                'is_top': True
-            })
-            dir_list = ['cities']
-        self.response.write(base_template.render({
-            'body': body, 'dir_list': dir_list}))
+class CitiesProjectsHandler(webapp2.RequestHandler):
+  def get(self, typ, item):
+    is_top = item != None
+    if typ == 'cities':
+      metas = CITY_METAS
+      items = CITIES
+    else:
+      metas = PROJECT_METAS
+      items = PROJECTS
 
+    if is_top:
+      frame_template = GetTemplate('%s/frame.html' % typ)
+      type_template = GetTemplate('%s/%s.html' % (typ, item))
+      info_template = GetTemplate('%s/info_frame.html' % typ)
+      info_body = info_template.render({
+        'description': type_template.render(),
+        'meta': metas[item],
+        'item_id': item
+       })
+      body = frame_template.render({
+          'info': info_body,
+          'item_id': item,
+          'items': [metas[i] for i in items]
+      })
+      dir_list = [typ, item]
+    else:
+      frame_template = GetTemplate('%s/frame.html' % typ)
+      body = frame_template.render({
+        'info': '',
+        'item_id': '',
+        'items': [metas[i] for i in items],
+        'is_top': True
+      })
+      dir_list = [typ]
+    self.response.write(WrapWithBaseTemplate(body, dir_list))
 
-class ProjectsHandler(webapp2.RequestHandler):
-  def get(self, project):
-    print '|'.join([p['id'] for p in PROJECTS])
-    is_projcet_top = project != ''
-    base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-    frame_template = JINJA_ENVIRONMENT.get_template('templates/projects/frame.html')
-    project_template = JINJA_ENVIRONMENT.get_template('templates/projects/%s.html' % project)
-    project_body = project_template.render()
-    frame_body = frame_template.render({
-      'project_id': project,
-      'projects': PROJECTS,
-      'description': project_body,
-      'meta': PROJECT_METAS[project]
-    })
-    dir_list = ['projects', project]
-    self.response.write(base_template.render({
-        'body': frame_body,
-        'dir_list': dir_list
-    }))
 
 class AboutHandler(webapp2.RequestHandler):
     def get(self, topic):
-        base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-        frame_template = JINJA_ENVIRONMENT.get_template('templates/about/frame.html')
-        blog_template = JINJA_ENVIRONMENT.get_template(
-            'templates/about/' + topic + '.html')
+        base_template = GetTemplate('base.html')
+        frame_template = GetTemplate('about/frame.html')
+        blog_template = GetTemplate('about/' + topic + '.html')
         body = blog_template.render()
         frame = frame_template.render({'body': body})
-        self.response.write(base_template.render({
-          'body': frame,
-          'dir_list': ['about', topic]
-        }))
+        self.response.write(WrapWithBaseTemplate(frame, ['about', topic]))
+
 
 class PeopleHandler(webapp2.RequestHandler):
     def get(self):
-        base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-        people_template = JINJA_ENVIRONMENT.get_template('templates/people.html')
+        base_template = GetTemplate('base.html')
+        people_template = GetTemplate('people.html')
         people_body = people_template.render()
-        self.response.write(base_template.render({
-            'body': people_body,
-            'dir_list': ['people']
-        }))
+        self.response.write(WrapWithBaseTemplate(people_body, ['people']))
+
 
 class SponsorsHandler(webapp2.RequestHandler):
     def get(self):
-        base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-        sponsors_template = JINJA_ENVIRONMENT.get_template('templates/sponsors.html')
+        base_template = GetTemplate('base.html')
+        sponsors_template = GetTemplate('sponsors.html')
         sponsors_body = sponsors_template.render()
-        self.response.write(base_template.render({
-            'body': sponsors_body,
-            'dir_list': ['sponsors']
-        }))
+        self.response.write(WrapWithBaseTemplate(sponsors_body, ['sponsors']))
+
 
 class TestCityHandler(webapp2.RequestHandler):
     def get(self):
-        base_template = JINJA_ENVIRONMENT.get_template('templates/base.html')
-        test_city_template = JINJA_ENVIRONMENT.get_template(
-          'templates/cities/test_city_tokyo.html')
-        info_template = JINJA_ENVIRONMENT.get_template('templates/cities/info_frame.html')
-        tokyo_template = JINJA_ENVIRONMENT.get_template('templates/cities/tokyo.html')
+        base_template = GetTemplate('base.html')
+        test_city_template = GetTemplate('/cities/test_city_tokyo.html')
+        info_template = GetTemplate('cities/info_frame.html')
+        tokyo_template = GetTemplate('cities/tokyo.html')
         info = info_template.render({
           'description': tokyo_template.render(),
           'meta': CITY_METAS['tokyo'],
-          'city_id': 'tokyo',
-          'city_label': 'Tokyo'
+          'item_id': 'tokyo',
         })
         body = test_city_template.render({
           'info': info
         })
-        self.response.write(base_template.render({
-            'body': body,
-            'dir_list': ['test_city_tokyo']
-        }))
+        self.response.write(WrapWithBaseTemplate(body, ['test_city_tokyo']))
+
 
 
 app = webapp2.WSGIApplication([
@@ -406,12 +421,8 @@ app = webapp2.WSGIApplication([
     webapp2.Route(r'/about/', webapp2.RedirectHandler, defaults={'_uri':
         '/about/wxii', '_code': 301}),
     (r'/about/(cities|projects|wxii|faq|rules)', AboutHandler),
-    (r'/cities/*(%s)?' % '|'.join(CITIES), CitiesHandler),
-    webapp2.Route(r'/projects', webapp2.RedirectHandler, defaults={'_uri':
-        '/projects/musicians', '_code': 301}),
-    webapp2.Route(r'/projects/', webapp2.RedirectHandler, defaults={'_uri':
-        '/projects/musicians', '_code': 301}),
-    (r'/projects/(%s)' % '|'.join([project['id'] for project in PROJECTS]), ProjectsHandler),
+    (r'/(cities)/*(%s)?' % '|'.join(CITIES), CitiesProjectsHandler),
+    (r'/(projects)/*(%s)?' % '|'.join(PROJECTS), CitiesProjectsHandler),
     ('/people', PeopleHandler),
     ('/sponsors', SponsorsHandler),
     ('/test_city_tokyo', TestCityHandler),
